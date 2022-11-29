dc39ff09263c6a909f7a56780632ed99811c8443
Raisul Showmin <46886381+rishowmin@users.noreply.github.com>
2022-11-30 02:28:18 +0600

    Check Availability New

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -31,14 +31,28 @@ class HomeController extends Controller
         $total_adults = $request->adults;
         $total_childs = $request->children;
 
-        $available_rooms = DB::select("SELECT * FROM hb_rooms WHERE id NOT IN (SELECT room_id FROM hb_bookings WHERE '$checkin_date' AND '$checkout_date' BETWEEN checkin_date AND checkout_date) AND max_adults >= '$total_adults' AND max_childs >= '$total_childs'");
-        
+        // $available_rooms = DB::select("SELECT * FROM hb_rooms WHERE id NOT IN (SELECT room_id FROM hb_bookings WHERE '$checkin_date' AND '$checkout_date' BETWEEN checkin_date AND checkout_date) AND max_adults >= '$total_adults' AND max_childs >= '$total_childs'");
+
+        $available_rooms = Room::whereNotIn('id', function($query) use ($checkin_date, $checkout_date) {
+                            $query->from('hb_bookings')
+                                ->select('room_id')
+                                ->where('checkin_date', '<=', $checkout_date)
+                                ->where('checkout_date', '>=', $checkin_date);
+                            })
+                            ->where('max_adults', '>=', $total_adults)
+                            ->where('max_childs', '>=', $total_childs)
+                            ->get();
+
+        // $available_rooms = Room::whereNotIn('id', function($query) use ($checkin_date, $checkout_date) {
+        //     $query->select('room_id')
+        //         ->from('hb_bookings')
+        //         ->where('checkin_date', '<=', $checkout_date)
+        //         ->where('checkout_date', '>=', $checkin_date);
+        // })
+        // ->where('max_adults', '>=', $total_adults)
+        // ->where('max_childs', '>=', $total_childs)
+        // ->get();
+
         return view('frontend.available-rooms', compact('available_rooms'));
     }
-
-    // public function availableRooms(Request $request, $checkin_date)
-    // {
-    //     $available_rooms = DB::select("SELECT * FROM hb_rooms WHERE id NOT IN (SELECT room_id FROM hb_bookings WHERE '$checkin_date' BETWEEN checkin_date AND checkout_date)");
-    //     return view('frontend.available-rooms', compact('available_rooms'));
-    // }
 }
